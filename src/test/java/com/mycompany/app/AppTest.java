d3d27425faac785977db686f6222a2191cbb3692
Ashish Kumar <admin@localhost.localdomain>
2020-10-27 04:54:53 -0400

    test 123

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World, I am running My First Maven Project!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World, I am running My First Maven Project for test!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
